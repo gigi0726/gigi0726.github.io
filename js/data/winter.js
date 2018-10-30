7a0488d0ba0b55311639fc87dd4dda9731a5c89b
gigi0726 <44458156+gigi0726@users.noreply.github.com>
2018-10-30 17:19:55 +0800

    create winter.js

---
+++ js/data/winter.js
@@ -0,0 +1,33 @@
+var data_winter = {     
+    'count': 5,
+    
+    'data1': {         
+        'type': 2,
+        'title': '寒修1',
+        'content': '123'
+    },
+        
+    'data2': {
+        'type': 2,
+        'title': '寒修2',
+        'content': '123'
+    },
+    
+    'data3': {
+        'type': 2,
+        'title': '寒修3',
+        'content': '123'
+    },
+    
+    'data4': {
+        'type': 2,
+        'title': '寒修4',
+        'content': '123'
+    },
+    
+    'data5': {
+        'type': 2,
+        'title': '寒修5',
+        'content': '123'
+    }
+}
