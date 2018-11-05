d3ee560a131a7adc5280d3aa273c888fd5f3aa47
gigi0726 <44458156+gigi0726@users.noreply.github.com>
2018-11-05 13:55:15 +0800

    update summer.js

--- js/data/summer.js
+++ js/data/summer.js
@@ -3,31 +3,31 @@ var data_summer = {
     
     'data1': {         
         'type': 2,
-        'title': '暑修1',
-        'content': '123'
+        'title': '徵暑修國文（上）',
+        'content': '發文日期2018/5/23<br>1.目前詢問系辦可10人開課<br>2.系辦預計七月第二個星期開課<br>3.可直接到中文系系辦詢問<br>4.可加以下群組'
     },
         
     'data2': {
         'type': 2,
-        'title': '暑修2',
-        'content': '123'
+        'title': '徵暑修外英（上）（下）',
+        'content': '發文日期2018/5/21<br>1.英語系官網有表單，會看人數決定開班 <br>2.時間為表單上面公告<br>3.去英文系辦問過，如果暑假開外英，會是三學分的課，假設你學制的英文是二的話，多的好像會算在你的選修學分內<br>4.外英（上）缺7人<br>外英（下）缺5人<br>5.外英（下）<br>Line群組群組：https://m.me/join/Aba7nazRPkKJPjvV'
     },
     
     'data3': {
         'type': 2,
-        'title': '暑修3',
-        'content': '123'
+        'title': '徵暑修體育',
+        'content': '發文日期2018/5/21<br>1.目前缺6人<br>2.去體育室填單，滿額即開課<br>3.體育群組如下：<br>https://line.me/R/ti/g/DNXfHqSp-g'
     },
     
     'data4': {
         'type': 2,
-        'title': '暑修4',
-        'content': '123'
+        'title': '徵暑修統計學（上）（下）',
+        'content': '發文日期2018/6/21<br>1.目前上學期已有13人、下學期14人<br>2.可以填表單：https://docs.google.com/forms/d/e/1FAIpQLSc2f37Kbcxh-yXRdUafiqvaQWUn5e1p8fbRy67ubrFMfCO-fQ/viewform'
     },
-    
-    'data5': {
+        
+    'data5': {         
         'type': 2,
-        'title': '暑修5',
-        'content': '123'
+        'title': '徵暑修傳播理論',
+        'content': '發文日期2018/6/3<br>1.目前缺三人<br>2.可以加line群組：https://line.me/R/ti/g/pHI8bVf6Bw'
     }
-}
+};
